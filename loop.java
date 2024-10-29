85a4ee2002d7bf83af13a03ebece551901071889
kay-099 <mwanzakay6@gmail.com>
2024-10-29 16:02:03 +0200

    This is the second commit

--- loop.java
+++ loop.java
@@ -1,7 +1,17 @@
 public class loop {
+    public String sound(int eNumber){
+        switch (eNumber) {
+            case 1:
+                return "this is the day";
+            case 2:
+                return "this is the night";
+        }
+        return null;
+
+    }
     public static void main(String[] args){
         for(int i= 0; i < 5; ++i){
-            System.out.println("Hello from oasis");
+            System.out.println("Good day from oasis");
         }
     }
 }
