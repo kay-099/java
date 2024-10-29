3d5a76550ab7362f6026ffa7304f4c3a66fb862b
kay-099 <mwanzakay6@gmail.com>
2024-10-29 15:41:27 +0200

    Added the ifStatment.java file, i have not compiled it yet

---
+++ ifStatment.java
@@ -0,0 +1,8 @@
+public class ifStatment {
+    public static void main(String[] args){
+        int i = 0;
+        if(i != 1){
+            System.out.println("An error ocurred");
+        }
+    }
+}
--- loop.java
+++ loop.java
@@ -1,7 +1,13 @@
 public class loop {
     public static void main(String[] args){
-        for(int i= 0; i < 5; ++i){
+        for(int i = 1; i <= 5; i++){
             System.out.println("Hello from oasis");
+            if(i == 5){
+                System.out.println("This is the last line");
+            }
+            else{
+                System.out.println("This is not the last line");
+            }
         }
     }
 }
